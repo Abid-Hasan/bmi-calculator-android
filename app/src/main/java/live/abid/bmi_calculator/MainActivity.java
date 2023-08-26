828d094dbd1f2d094d167752aa22c509c869e2d9
Abid-Hasan <ahsnigdho@gmail.com>
2023-08-26 14:17:31 +0600

    Do UI

---
+++ app/src/main/java/live/abid/bmi_calculator/MainActivity.java
@@ -0,0 +1,56 @@
+package live.abid.bmi_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioButton radioButtonMale;
+    private RadioButton radioButtonFemale;
+    private EditText editTextAge;
+    private EditText editTextFeet;
+    private EditText editTextInches;
+    private EditText editTextWeight;
+    private Button buttonCalculate;
+    private TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonClickListener();
+    }
+
+
+    private void findViews() {
+        radioButtonMale = findViewById(R.id.radio_button_male);
+        radioButtonFemale = findViewById(R.id.radio_button_female);
+        editTextAge = findViewById(R.id.edit_text_age);
+        editTextFeet = findViewById(R.id.edit_text_feet);
+        editTextInches = findViewById(R.id.edit_text_inches);
+        editTextWeight = findViewById(R.id.edit_text_weight);
+        buttonCalculate = findViewById(R.id.button_calculate);
+        resultText = findViewById(R.id.text_view_result);
+    }
+
+    private void setupButtonClickListener() {
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi();
+            }
+        });
+    }
+
+    private void calculateBmi() {
+        resultText.setText("Button clicked");
+    }
+}
